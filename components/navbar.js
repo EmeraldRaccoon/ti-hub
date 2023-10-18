73eef42d0a8f943bc4a63bb9d8ae8cbf39f72f41
James Haydon <james.haydon@siemens.com>
2023-10-18 11:50:49 +0100

    initial - basic factions update

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+// Fetch and insert the navigation bar
+fetch("../components/navbar.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("navbar-placeholder").innerHTML = data;
+  });
+
+// Dropdown menu search
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
